68dad70c59740f13287c2da590d238fe51ab923f
zhuHuaJian <935625755@qq>
2017-04-12 15:21:11 +0800

    改包名，改applicationId，改Demo为Live

---
+++ app/src/main/java/cn/ucai/live/net/OnClompleterListener.java
@@ -0,0 +1,10 @@
+package cn.ucai.live.net;
+
+import cn.ucai.live.utils.OkHttpUtils;
+
+/**
+ * Created by Administrator on 2017/4/12.
+ */
+
+public interface OnClompleterListener<T> extends OkHttpUtils.OnCompleteListener<T> {
+}
---
+++ app/src/main/java/cn/ucai/live/utils/MFGT.java
@@ -0,0 +1,78 @@
+package cn.ucai.live.utils;
+
+/**
+ * Created by Moon on 2017/3/16.
+ */
+
+public class MFGT {
+//    public static void startActivity(Activity activity, Class cls) {
+//        activity.startActivity(new Intent(activity, cls));
+//        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+//    }
+//
+//    public static void startActivity(Activity activity, Intent intent) {
+//        activity.startActivity(intent);
+//        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+//    }
+//
+//    public static void finish(Activity activity) {
+//        activity.finish();
+//        activity.overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+//    }
+
+//    public static void gotoMain(Activity activity) {
+//        startActivity(activity, MainActivity.class);
+//    }
+//
+//    public static void gotoBoutiqueChild(Context activity, BoutiqueBean bean) {
+//        activity.startActivity(new Intent(activity, BoutiqueChildActivity.class)
+//                .putExtra(I.NewAndBoutiqueGoods.CAT_ID, bean.getId())
+//                .putExtra(I.Boutique.TITLE, bean.getTitle()));
+//    }
+//
+//    public static void gotoGoodsDetails(Context activity, int goodsId) {
+//        startActivityForResult((Activity) activity,new Intent(activity, GoodsDetailsActivity.class)
+//                .putExtra(I.Goods.KEY_GOODS_ID,goodsId),I.REQUEST_CODE_COLLECTED);
+//
+//    }
+//
+//
+//    public static void gotoCategoryChildActivity(Context context, int goodsId, String groupName,
+//                                                 ArrayList<CategoryChildBean> list) {
+//        context.startActivity(new Intent(context, CategoryChildActivity.class)
+//        .putExtra(I.NewAndBoutiqueGoods.CAT_ID,goodsId)
+//        .putExtra(I.CategoryGroup.NAME,groupName)
+//        .putExtra(I.CategoryChild.DATA,list));
+//    }
+//
+//    public static void gotoLogin(Activity activity,int requestCode) {
+//        startActivityForResult(activity, new Intent(activity,LoginActivity.class),requestCode);
+//    }
+//
+//    public static void gotoRegister(Activity activity) {
+//        startActivityForResult(activity,new Intent(activity,RegisterActivity.class),
+//                I.REQUEST_CODE_REGISTER);
+//    }
+//
+//    public static void startActivityForResult(Activity activity, Intent intent, int requestCode) {
+//        activity.startActivityForResult(intent, requestCode);
+//        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+//    }
+//
+//    public static void gotoSettingActivity(Activity activity) {
+//        startActivity(activity,SettingActivity.class);
+//    }
+//
+//    public static void gotoUpdateNick(Activity activity) {
+//        startActivity(activity, UpdateNickActivity.class);
+//    }
+//
+//    public static void gotoCollectList(Activity activity) {
+//        startActivity(activity, CollectsActivity.class);
+//    }
+//
+//    public static void gotoOrder(Activity activity, int price) {
+//        startActivity(activity,new Intent(activity, OrderActivity.class)
+//                .putExtra(I.ORDER_BUY_PRICE,price));
+//    }
+}
