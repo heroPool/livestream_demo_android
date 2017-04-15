666da477159c4f4d9e4135fd072c394545b237c7
zhuHuaJian <935625755@qq>
2017-04-15 17:41:02 +0800

    创建openhelper和giftdao

--- app/src/main/java/cn/ucai/live/data/local/GiftDao.java
+++ app/src/main/java/cn/ucai/live/data/local/GiftDao.java
@@ -4,64 +4,27 @@ import com.hyphenate.easeui.domain.EaseUser;
 import com.hyphenate.easeui.domain.User;
 
 import java.util.List;
-import java.util.Map;
-
-import cn.ucai.live.data.model.Gift;
 
 /**
  * Created by Administrator on 2017/4/14.
  * private Integer id;
-
- private String gname;
-
- private String gurl;
-
- private Integer gprice;
-
- public Integer getId() {
- return id;
- }
+ * <p>
+ * private String gname;
+ * <p>
+ * private String gurl;
+ * <p>
+ * private Integer gprice;
+ * <p>
+ * public Integer getId() {
+ * return id;
+ * }
  */
 
 public class GiftDao {
-    public static final String TABLE_NAME = "Gifts";
+    public static final String GIFT_TABLE_NAME = "t_live_gift";
+    public static final String GIFT_COLUMN_ID = "m_gift_id";
+    public static final String GIFT_COLUMN_NAME = "m_gift_name";
+    public static final String GIFT_COLUMN_URL = "m_gift_url";
+    public static final String GIFT_COLUMN_PRICE = "m_gift_price";
 
-
-//    public static final String USER_TABLE_NAME = "t_superwechat_user";
-//    public static final String USER_COLUMN_NAME = "m_user_name";
-//    public static final String USER_COLUMN_NICK = "m_user_nick";
-//    public static final String USER_COLUMN_AVATAR_ID = "m_user_avatar_id";
-//    public static final String USER_COLUMN_AVATAR_PATH = "m_user_avatar_path";
-//    public static final String USER_COLUMN_AVATAR_SUFFIX = "m_user_avatar_suffix";
-//    public static final String USER_COLUMN_AVATAR_TYPE = "m_user_avatar_type";
-//    public static final String USER_COLUMN_AVATAR_LASTUPDATE_TIME = "m_user_avatar_lastupdate_time";
-
-
-    public static final String COLUMN_NAME_ID = "gname";
-    public static final String COLUMN_NAME_NICK = "gurl";
-    public static final String COLUMN_NAME_AVATAR = "avatar";
-    public Map<String, Gift> getAppContactList() {
-
-        return LiveDBManager.getInstance().getAppContactList();
-    }
-
-    /**
-     * delete a contact
-     * @param username
-     */
-    public void deleteAppContact(String username){
-        LiveDBManager.getInstance().deleteAppContact(username);
-    }
-
-    /**
-     * save a contact
-     * @param user
-     */
-    public void saveAppContact(User user){
-        LiveDBManager.getInstance().saveAppContact(user);
-    }
-
-    public void saveContactList(List<EaseUser> contactList) {
-        LiveDBManager.getInstance().saveContactList(contactList);
-    }
 }
