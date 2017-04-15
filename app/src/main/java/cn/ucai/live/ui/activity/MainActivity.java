bd01357dbf6475c30aad595ffba08df7b1c08593
zhuHuaJian <935625755@qq>
2017-04-15 16:06:51 +0800

    修改liveapplication文件，退出登录后删除数据

--- app/src/main/java/cn/ucai/live/ui/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/MainActivity.java
@@ -5,21 +5,26 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+
 import com.easemob.livedemo.R;
 import com.hyphenate.EMCallBack;
 import com.hyphenate.chat.EMClient;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.live.LiveHelper;
+
 public class MainActivity extends BaseActivity {
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
         getActionBarToolbar().setNavigationOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, SearchActivity.class));
             }
         });
@@ -31,41 +36,48 @@ public class MainActivity extends BaseActivity {
         processConflictIntent(getIntent());
     }
 
-    @OnClick(R.id.floatingActionButton) void createLiveRoom() {
+    @OnClick(R.id.floatingActionButton)
+    void createLiveRoom() {
         startActivity(new Intent(this, CreateLiveRoomActivity.class));
     }
 
-    @OnClick(R.id.txt_logout) void logout() {
-        EMClient.getInstance().logout(false, new EMCallBack() {
-            @Override public void onSuccess() {
+    @OnClick(R.id.txt_logout)
+    void logout() {
+        LiveHelper.getInstance().logout(false, new EMCallBack() {
+            @Override
+            public void onSuccess() {
                 finish();
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
 
-            @Override public void onError(int i, String s) {
+            @Override
+            public void onError(int i, String s) {
 
             }
 
-            @Override public void onProgress(int i, String s) {
+            @Override
+            public void onProgress(int i, String s) {
 
             }
         });
     }
 
-    @Override protected void onNewIntent(Intent intent) {
+    @Override
+    protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         processConflictIntent(intent);
     }
 
     private void processConflictIntent(Intent intent) {
-        if(intent.getBooleanExtra("conflict", false)) {
+        if (intent.getBooleanExtra("conflict", false)) {
             EMClient.getInstance().logout(false, null);
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle(R.string.prompt)
                     .setMessage("账户已在别处登录！")
                     .setCancelable(false)
                     .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                        @Override public void onClick(DialogInterface dialog, int which) {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
                             finish();
                             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
